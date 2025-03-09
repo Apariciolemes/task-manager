09bcb9182c371581da5adf9a2dfd058e16f12c03
Aparicio Leme Neto <daniel.silvado@terceirizados.farmarcas.com.br>
2025-03-08 21:49:40 -0300

    chore: remove config autoLoadEntities

--- src/database/root-config.ts
+++ src/database/root-config.ts
@@ -9,5 +9,4 @@ export const dbRoot = TypeOrmModule.forRoot({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  autoLoadEntities: true,
 });
